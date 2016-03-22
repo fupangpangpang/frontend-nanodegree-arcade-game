ffbe77c817c58f5d0343f82a4529cb71b86b507c
fupangpangpang <jason.wfu@gmail.com>
2016-03-21 22:16:33 -0400

    update of app.js

--- js/app.js
+++ js/app.js
@@ -6,30 +6,66 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = Math.floor((Math.random() * 5));
+    this.y = Math.floor((Math.random() * 6));
+    this.speed = 1;
 };
-
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x ;
+    this.y = this.x ;
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x *101, this.y *83);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = Math.floor((Math.random() * 5));
+    this.y = Math.floor((Math.random() * 6));
+    this.speed = 1;
+}
 
+Player.prototype.update = function(dt) {
+    this.x = this.x ;
+    this.y = this.y ;
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x *101, this.y *83);
+};
+
+Player.prototype.handleInput = function(command) {
+    if (command === 'left') {
+        this.x -= 1;
+    } else if (command === 'up') {
+        this.y -= 1;
+    } else if (command === 'right') {
+        this.x += 1;
+    } else if (command === 'down') {
+        this.y += 1;
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+var E1 = new Enemy();
+var E2 = new Enemy();
+allEnemies.push(E1);
+allEnemies.push(E2);
 
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
